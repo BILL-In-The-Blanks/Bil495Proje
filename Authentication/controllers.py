b546248a3c107f50a2be9f5b110a19e51b0e30f9
Toygar KOCA <43719958+ttoygarkoca@users.noreply.github.com>
2019-10-21 20:53:29 +0300

    Kullanıcı kaydi ve giriş kontrolü

--- Authentication/controllers.py
+++ Authentication/controllers.py
@@ -15,3 +15,20 @@ def add_user():
     db.session.commit()
     return user_schema.jsonify(new_user)
 
+
+#kayitli kullanicinin giris bilgilerini kontrol etme
+@mod_auth.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    email = data['email']
+    password = data['password']
+    user = Users.query.filter_by(email=email).first()
+    if not user:
+        return make_response('User not found', 401)
+    if check_password_hash(user.password, password):
+        token = jwt.encode({'userid': user.user_id}, app.config['SECRET_KEY'])
+        user.user_token = token
+        db.session.commit()
+        out = jsonify(success=True,token=token.decode('utf-8'))
+        return out
+    return make_response('Password is incorrect', 401)
