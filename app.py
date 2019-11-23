8189fbc80219b0a39aa9d4754f28e044bf708aa6
BaturayOnural <baturayonural@gmail.com>
2019-11-23 23:09:34 +0300

    Fixed #17. Search functionality is now available.

--- app.py
+++ app.py
@@ -51,42 +51,7 @@ class Users(db.Model):                  #users icin ikinci database class'i. bin
         return '<Receipt %r>' % self.id
 
 
-@app.route('/<int:user_id>', methods=['POST', 'GET', 'PUT'])
-def index(user_id):
-    if request.method == 'POST':
-        receipt_name = request.form['name']
-        receipt_location = request.form['location']
-        receipt_tag = request.form['tag'] 
-        receipt_total_cost = request.form['total_cost']
-
-       
-        file = request.files['file']
-
-        
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(PATH + UPLOAD_FOLDER, filename))
-            receipt_photo_path = os.path.join(UPLOAD_FOLDER, filename)
-        else:
-            receipt_photo_path = ""
-            
-
-        new_receipt = Receipt(name=receipt_name,total_cost=receipt_total_cost, location=receipt_location, tag=receipt_tag, photo_path=receipt_photo_path)
-
-        try:
-            db.session.add(new_receipt)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your receipt'
-
-    else:
-        receipts = Receipt.query.order_by(Receipt.date_created).all()
-        return render_template('index.html', receipts=receipts)
-
-@app.route('/<int:user_id>/', methods=['POST', 'GET'])
+@app.route('/<int:user_id>/', methods=['GET'])
 def index_user(user_id):
 
     
@@ -94,6 +59,40 @@ def index_user(user_id):
     user = Users.query.get_or_404(user_id)
 
     return render_template('index.html', receipts=receipts, user=user)
+@app.route('/<int:user_id>/', methods=['POST'])
+def index_user1(user_id):
+
+	print("DONE")
+	user = Users.query.get_or_404(user_id)
+
+	receipt_name = request.form['name']
+	receipt_location = request.form['location']
+	receipt_tag = request.form['tag'] 
+	receipt_total_cost = request.form['total_cost']
+
+
+	file = request.files['file']
+
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(PATH + UPLOAD_FOLDER, filename))
+		receipt_photo_path = os.path.join(UPLOAD_FOLDER, filename)
+	else:
+		receipt_photo_path = ""
+
+
+	new_receipt = Receipt(name=receipt_name,total_cost=receipt_total_cost, location=receipt_location, tag=receipt_tag, photo_path=receipt_photo_path, belonging_user_id=user.user_id)
+
+
+	try:
+
+		print("DONE2")
+		db.session.add(new_receipt)
+		db.session.commit()
+		receipts = Receipt.query.filter_by(belonging_user_id = user_id).all()
+		return render_template('index.html', receipts=receipts, user=user)
+	except:
+		return 'There was an issue adding your receipt'
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -210,48 +209,53 @@ def enter_details(id):
     else:
         return render_template('update_2.html', receipt=receipt)
 
-@app.route('/search',  methods=['GET', 'POST'])
-def search_results():
+@app.route('/search/<int:user_id>',  methods=['GET', 'POST'])
+def search_results(user_id):
     if request.method == 'POST':
-        search_string = request.form['search']
-        searchBox = request.form.getlist('searchBox')
+    	receipts = Receipt.query.filter_by(belonging_user_id = user_id)
+    	search_string = request.form['search']
+    	searchBox = request.form.getlist('searchBox')
 
-        if (len(searchBox)==0):
-            receipts1 = Receipt.query.filter(Receipt.name.like(search_string + "%")).all()   
-            receipts2 = Receipt.query.filter(Receipt.location.like(search_string + "%")).all()
-            receipts3 = Receipt.query.filter(Receipt.tag.like(search_string + "%")).all()   
-            receiptsFinal = receipts1 + receipts2 + receipts3   
-        elif(len(searchBox)==1):
-            if(searchBox[0]=="1"):
-                receipts1 = Receipt.query.filter(Receipt.name.like(search_string + "%")).all()
-                receiptsFinal = receipts1
-            elif(searchBox[0]=="2"):
-                receipts2 = Receipt.query.filter(Receipt.location.like(search_string + "%")).all()
-                receiptsFinal = receipts2
-            elif(searchBox[0]=="3"):
-                receipts3 = Receipt.query.filter(Receipt.tag.like(search_string + "%")).all()  
-                receiptsFinal = receipts3 
-        elif(len(searchBox)==2):
-            if(searchBox[0]=="1" and searchBox[1]=="2"):
-                receipts1 = Receipt.query.filter(Receipt.name.like(search_string + "%")).all()
-                receipts2 = Receipt.query.filter(Receipt.location.like(search_string + "%")).all()
-                receiptsFinal = receipts1 + receipts2
-            if(searchBox[0]=="1" and searchBox[1]=="3"):
-                receipts1 = Receipt.query.filter(Receipt.name.like(search_string + "%")).all()
-                receipts3 = Receipt.query.filter(Receipt.tag.like(search_string + "%")).all()  
-                receiptsFinal = receipts1 + receipts3 
-            if(searchBox[0]=="2" and searchBox[1]=="3"):
-                receipts3 = Receipt.query.filter(Receipt.tag.like(search_string + "%")).all() 
-                receipts2 = Receipt.query.filter(Receipt.location.like(search_string + "%")).all() 
-                receiptsFinal = receipts2 + receipts3 
-        elif(len(searchBox)==3):
-            receipts1 = Receipt.query.filter(Receipt.name.like(search_string + "%")).all()   
-            receipts2 = Receipt.query.filter(Receipt.location.like(search_string + "%")).all()
-            receipts3 = Receipt.query.filter(Receipt.tag.like(search_string + "%")).all()   
-            receiptsFinal = receipts1 + receipts2 + receipts3
+    	if(search_string==""):
+	       	return render_template('index.html', receipts = receipts.all(), user=Users.query.get_or_404(user_id))
+    	if(len(searchBox)==0):
+	        	
+        	receipts1 = Receipt.query.filter(Receipt.name.like(search_string + "%")) 
+	        receipts2 = Receipt.query.filter(Receipt.location.like(search_string + "%"))
+	        receipts3 = Receipt.query.filter(Receipt.tag.like(search_string + "%"))   
+	        receiptsFinal = receipts1.union(receipts2).union(receipts3)
+	        receiptsFinal = receiptsFinal.intersect(receipts).all()
+    	elif(len(searchBox)==1):
+    		if(searchBox[0]=="1"):
+		        receipts1 = Receipt.query.filter(Receipt.name.like(search_string + "%"))
+		        receiptsFinal = receipts1.intersect(receipts).all()
+    		elif(searchBox[0]=="2"):
+		        receipts2 = Receipt.query.filter(Receipt.location.like(search_string + "%"))
+		        receiptsFinal = receipts2.intersect(receipts).all()
+    		elif(searchBox[0]=="3"):
+		        receipts3 = Receipt.query.filter(Receipt.tag.like(search_string + "%"))
+		        receiptsFinal = receipts3.intersect(receipts).all() 
+    	elif(len(searchBox)==2):
+	        if(searchBox[0]=="1" and searchBox[1]=="2"):
+	            receipts1 = Receipt.query.filter(Receipt.name.like(search_string + "%"))
+	            receipts2 = Receipt.query.filter(Receipt.location.like(search_string + "%"))
+	            receiptsFinal = (receipts1.union(receipts2)).intersect(receipts).all()
+	        if(searchBox[0]=="1" and searchBox[1]=="3"):
+	            receipts1 = Receipt.query.filter(Receipt.name.like(search_string + "%"))
+	            receipts3 = Receipt.query.filter(Receipt.tag.like(search_string + "%"))
+	            receiptsFinal = (receipts1.union(receipts3)).intersect(receipts).all()
+	        if(searchBox[0]=="2" and searchBox[1]=="3"):
+	            receipts3 = Receipt.query.filter(Receipt.tag.like(search_string + "%"))
+	            receipts2 = Receipt.query.filter(Receipt.location.like(search_string + "%"))
+	            receiptsFinal = (receipts2.union(receipts3)).intersect(receipts).all()
+    	elif(len(searchBox)==3):
+	        receipts1 = Receipt.query.filter(Receipt.name.like(search_string + "%")) 
+	        receipts2 = Receipt.query.filter(Receipt.location.like(search_string + "%"))
+	        receipts3 = Receipt.query.filter(Receipt.tag.like(search_string + "%"))   
+	        receiptsFinal = (((receipts1.union(receipts2)).union(receipts3)).intersect(receipts)).all()
 
-                   
-        return render_template('results.html', receipts=receiptsFinal)
+	               
+    	return render_template('index.html', receipts=receiptsFinal, user=Users.query.get_or_404(user_id))
 
 
 if __name__ == "__main__":
